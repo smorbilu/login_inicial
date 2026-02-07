af4fde021021565c3ddec2d3fc4f614793ea540c
smorbilu <luisrivasdeleon2005@gmail.com>
2026-02-06 18:38:10 -0600

    cambio en codigo

--- my-app/src/app/components/login/login.component.ts
+++ my-app/src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Login } from 'src/app/interfaces/loginDto';
-import { LoginService } from 'src/app/services/login/login.service';
+
 
 @Component({
   selector: 'app-login',
@@ -8,28 +8,19 @@ import { LoginService } from 'src/app/services/login/login.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  
-  private loginService = inject(LoginService);
-
   username: string = '';
   password: string = '';
 
   login() {
     console.log('Usuario:', this.username);
     console.log('Password:', this.password);
+    const objectRequest: Login ={
+      username: this.username,
+      password: this.password 
+    };
 
-    if (this.username !== '' && this.password !== '') {
-      const objectRequest: Login = {
-        username: this.username,
-        password: this.password 
-      };
-
-      this.loginService.doLogin(objectRequest).subscribe(entry => {
-        if (entry) {
-          console.log("login exitoso");
-        }
-      });
-    }
+     
+    
 
   }
 }
